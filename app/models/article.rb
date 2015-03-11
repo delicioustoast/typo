748e79aad7c1608f0dbc920f513d3fb40ec73943
saasbook <saasbook@saasbook.(none)>
2015-03-10 22:17:07 -0300

    comments now transfer over

--- app/models/article.rb
+++ app/models/article.rb
@@ -79,7 +79,7 @@ class Article < Content
 
     # other_comments_list = Comment.where('article_id == ?', other_article.id)
     Comment.where('article_id == ?', other_article.id).find_each do |comment| 
-      comment.article_id = other_article.id
+      comment.article_id = self.id
       comment.save!
     end
   end
